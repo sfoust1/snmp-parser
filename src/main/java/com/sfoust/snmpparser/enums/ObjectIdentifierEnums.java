178eaec1d1db28ea0edbd7a0f26310b3db122ba7
sfoust1 <sfoust61@gmail.com>
2025-03-28 22:31:02 -0600

    Created a single test on reading a file that contains multiple object types

--- src/main/java/com/sfoust/snmpparser/enums/ObjectIdentifierEnums.java
+++ src/main/java/com/sfoust/snmpparser/enums/ObjectIdentifierEnums.java
@@ -1,7 +1,7 @@
 package com.sfoust.snmpparser.enums;
 
 public enum ObjectIdentifierEnums implements PatternEnumsInterface {
-	ROOT("^([a-zA-Z]+)\\s+OBJECT IDENTIFIER ::= \\{ ([a-zA-Z]+) ([0-9]+) \\}($|\\s+)"),
+	ROOT("^([a-zA-Z0-9]+)\\s+OBJECT IDENTIFIER ::= \\{ ([a-zA-Z0-9]+) ([0-9]+) \\}($|\\s+)"),
 	OBJECT("^([a-zA-Z0-9]+)\\s+OBJECT-TYPE($|\\s+)"),
 	SEQUENCE("^([a-zA-Z0-9]+)\\s+::= SEQUENCE($|.+)");
 	
